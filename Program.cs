035528ede282f7e05a275951148446db8b09214a
chandrasekhar20 <69673014+chandrasekhar20@users.noreply.github.com>
2020-09-14 10:09:09 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace demoproject
+namespace variablestypes
 {
     class Program
     {
-        int i;
-        bool b;
+        int x;
+        const float pi = 3.14f;
+        readonly bool flag;
+        public Program(int x,bool flag)
+        {
+            this.x = x;
+            this.flag = flag;
+        }
         static void Main(string[] args)
         {
-            Program p = new Program();
-            Console.WriteLine("value of i is" +p.i);
-            Console.WriteLine("value of b is" +p.b);
-
+            int y = 200;
+            Program p1 = new Program(10,true);
+            Program p2 = new Program(20,false);
+            Console.WriteLine(p1.x+"  "+p2.x);
+            Console.WriteLine(p1.flag + "   " + p2.flag);
+            p1.x = 200;
+            Console.WriteLine(y);
+            Console.WriteLine(pi);
             Console.ReadLine();
         }
     }
