f90276daa4828dc8660fa19c39f7df7bac099f6e
chandrasekhar20 <69673014+chandrasekhar20@users.noreply.github.com>
2020-09-14 10:13:38 +0530

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testoperator
+{
+    class Matrix
+    {
+        int a, b, c, d;
+        public Matrix(int a,int b,int c,int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+
+        }
+        public override string ToString()
+        {
+            return a + " " + b + "\n" + c + " " + d + "\n";
+        }
+        public static Matrix operator +(Matrix obj1,Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a + obj2.a, obj1.b + obj2.b, obj1.c + obj2.c, obj1.d + obj2.d);
+            return obj;
+        }
+        public static Matrix operator -(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a - obj2.a, obj1.b - obj2.b, obj1.c - obj2.c, obj1.d - obj2.d);
+            return obj;
+        }
+    }
+    class testmatrix
+    {
+        static void Main()
+        {
+            Matrix m1 = new Matrix(1, 2, 3, 4);
+            Matrix m2 = new Matrix(1, 2, 3, 4);
+            Matrix m3 = m1 + m2;
+            Matrix m4 = m1 - m2;
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+            Console.WriteLine(m4);
+            Console.ReadLine();
+        }
+    }
+}
