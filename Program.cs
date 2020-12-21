01426a27a2fc0895d080b7b99ba191b56f48ad9e
chandrasekhar20 <69673014+chandrasekhar20@users.noreply.github.com>
2020-12-21 19:15:34 +0530

    commandline_demo

--- Program.cs
+++ Program.cs
@@ -4,94 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AbstractClassDemo
+namespace CommandLineArguments_Demo
 {
-     /*class University
-    {
-        public virtual void Method1() { }
-        public virtual void Method2() { }
-
-        public void Method3()
-        {
-
-        }
-        
-    }
-
-   abstract  class MyClass : University
-    {
-
-        abstract public  void Method4();
-        public override  void Method1()
-        {
-            
-        }
-
-        public override  void Method2()
-        {
-            
-        }
-
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            University u = new University();
-            MyClass my = new MyClass();
-        }
-    }*/
-     class One
-    {
-       public void OneMethod()
-        {
-
-        }
-
-        void AnimalSound()
-        {
-
-        }
-    }
-
-    class Two
-    {
-        public void OneMethod()
-        {
-
-        }
-    }
-    interface IAnimal
-    {
-               
-          void AnimalSound();
-                
-    }
-
-    interface IAnimalProperties
-    {
-        void AnimalMovement();
-    }
-
-    class Dog : One,IAnimal   
-    {
-        public void AnimalSound()
-        {
-          // dog barking sound   
-        }
-
-       public  void AnimalMovement()
-        {
-
-        }
-    }
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            
+            Console.WriteLine("No of command line arguments:" + args.Length);
+            foreach (string arg in args)
+                Console.WriteLine(arg);
+            Console.ReadLine();
         }
     }
 }
